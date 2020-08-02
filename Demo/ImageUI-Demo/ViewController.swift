0a360b73dbb069c9d25a83766fbf6f8f00deb98d
Alberto Saltarelli <a.saltarelli93@gmail.com>
2020-08-02 16:32:35 +0200

    Clean up

--- Demo/ImageUI-Demo/ViewController.swift
+++ Demo/ImageUI-Demo/ViewController.swift
@@ -11,12 +11,10 @@ import NeumorphicUI
 
 class ViewController: UIViewController {
     
-    @IBOutlet private weak var uiButton: UIButton!
     @IBOutlet private weak var control: NeumorphicControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        control.isEnabled = false
         control
             .cornerRadius(radius: .circle)
             .border(position: .inside, width: 25, color: .green)
@@ -24,6 +22,11 @@ class ViewController: UIViewController {
             .border(position: .inside, width: 75, color: .yellow)
             .border(position: .inside, width: 100, color: .orange)
             .border(position: .inside, width: 125, color: .blue)
+            .border(position: .inside, width: 25, color: .blue, state: .selected)
+            .border(position: .inside, width: 50, color: .orange, state: .selected)
+            .border(position: .inside, width: 75, color: .yellow, state: .selected)
+            .border(position: .inside, width: 100, color: .red, state: .selected)
+            .border(position: .inside, width: 125, color: .green, state: .selected)
     }
     
     @IBAction private func controlDidTap() {
