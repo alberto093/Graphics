9ee9ab5a48f24058af9168672c7d7f0c5da210ce
Alberto Saltarelli <a.saltarelli93@gmail.com>
2021-06-18 01:25:03 +0200

    Add neumorphic button examples

--- Demo/Graphics-Demo/Neumorphism/NeumorphicButtonsViewController.swift
+++ Demo/Graphics-Demo/Neumorphism/NeumorphicButtonsViewController.swift
@@ -11,22 +11,22 @@ import Graphics
 
 class NeumorphicButtonsViewController: UIViewController {
     
-    @IBOutlet private weak var titleButton: GraphicsButton!
-    @IBOutlet private weak var imageButton: GraphicsButton!
     @IBOutlet private weak var gradientButton: GraphicsButton!
+    @IBOutlet private weak var imageButton: GraphicsButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .demoBackground
         setupTitleButton()
+        setupImageButton()
     }
     
     private func setupTitleButton() {
-        titleButton.configuration.background = .solid(.demoBackground)
-        titleButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        gradientButton.configuration.background = .solid(.demoBackground)
+        gradientButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
         
-        titleButton
+        gradientButton
             .cornerRadius(radius: 20)
             .shadow(color: .darkShadow, offset: CGSize(width: 5, height: 5), blur: 15, opacity: 0.4)
             .shadow(color: .lightShadow, offset: CGSize(width: -5, height: -5), blur: 15)
@@ -37,12 +37,48 @@ class NeumorphicButtonsViewController: UIViewController {
             .shadow(.inner, color: .lightShadow, offset: CGSize(width: -10, height: -10), blur: 10, opacity: 0.7, state: .selected)
             .shadow(.inner, color: .darkShadow, offset: CGSize(width: 10, height: 10), blur: 10, opacity: 0.2, state: .selected)
         
-        titleButton.setTitleGradient(colors: [.red, .orange], for: .normal)
-        titleButton.setTitle("Graphics", for: .normal)
-        titleButton.titleLabel.font = .boldSystemFont(ofSize: 48)
+        gradientButton.setTitleGradient(colors: [.red, .orange], for: .normal)
+        gradientButton.setTitleGradient(colors: [.orange, .red], for: .selected)
+        gradientButton.setTitle("Graphics", for: .normal)
+        gradientButton.titleLabel.font = .boldSystemFont(ofSize: 48)
     }
     
-    @IBAction private func titleButtonDidTap() {
-        titleButton.isSelected.toggle()
+    private func setupImageButton() {
+        imageButton.configuration.background = .solid(.demoBackground)
+        imageButton.configuration.imageBackground = .solid(.demoBackground)
+        imageButton.imageEdgeInsets = UIEdgeInsets(top: 26, left: 18, bottom: 26, right: 18)
+        imageButton.tintColor = .purple
+        
+        imageButton
+            .cornerRadius(radius: .circle)
+            .shadow(color: .darkShadow, offset: CGSize(width: 5, height: 5), blur: 15, opacity: 0.4)
+            .shadow(color: .lightShadow, offset: CGSize(width: -5, height: -5), blur: 15)
+            .shadow(color: .darkShadow, offset: CGSize(width: 2, height: 2), blur: 10, opacity: 0.4, state: .highlighted)
+            .shadow(color: .lightShadow, offset: CGSize(width: -2, height: -2), blur: 10, state: .highlighted)
+            .shadow(.inner, color: .lightShadow, offset: CGSize(width: -14, height: -14), blur: 15, opacity: 0.7, state: [.highlighted, .selected])
+            .shadow(.inner, color: .darkShadow, offset: CGSize(width: 14, height: 14), blur: 15, opacity: 0.2, state: [.highlighted, .selected])
+            .shadow(.inner, color: .lightShadow, offset: CGSize(width: -10, height: -10), blur: 10, opacity: 0.7, state: .selected)
+            .shadow(.inner, color: .darkShadow, offset: CGSize(width: 10, height: 10), blur: 10, opacity: 0.2, state: .selected)
+        
+        imageButton.imageGraphicsItem
+            .cornerRadius(radius: .circle)
+            .shadow(color: .darkShadow, offset: CGSize(width: 5, height: 5), blur: 15, opacity: 0.4)
+            .shadow(color: .lightShadow, offset: CGSize(width: -5, height: -5), blur: 15)
+            .shadow(color: .darkShadow, offset: CGSize(width: 2, height: 2), blur: 10, opacity: 0.4, state: .highlighted)
+            .shadow(color: .lightShadow, offset: CGSize(width: -2, height: -2), blur: 10, state: .highlighted)
+            .shadow(.inner, color: .lightShadow, offset: CGSize(width: -14, height: -14), blur: 15, opacity: 0.7, state: [.highlighted, .selected])
+            .shadow(.inner, color: .darkShadow, offset: CGSize(width: 14, height: 14), blur: 15, opacity: 0.2, state: [.highlighted, .selected])
+            .shadow(.inner, color: .lightShadow, offset: CGSize(width: -10, height: -10), blur: 10, opacity: 0.7, state: .selected)
+            .shadow(.inner, color: .darkShadow, offset: CGSize(width: 10, height: 10), blur: 10, opacity: 0.2, state: .selected)
+        
+        imageButton.setImage(UIImage(systemName: "play.circle"), for: .normal)
+        imageButton.setImage(UIImage(systemName: "play.circle.fill"), for: .highlighted)
+        imageButton.setImage(UIImage(systemName: "play.circle.fill"), for: .selected)
+        imageButton.setImage(UIImage(systemName: "play.circle.fill"), for: [.highlighted, .selected])
+        imageButton.imageView.preferredSymbolConfiguration = UIImage.SymbolConfiguration(pointSize: 68, weight: .regular, scale: .large)
+    }
+    
+    @IBAction private func buttonDidTap(_ sender: UIControl) {
+        sender.isSelected.toggle()
     }
 }
